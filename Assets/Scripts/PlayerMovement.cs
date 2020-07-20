3e48947ed096b1f0e11ec7b80ea8d3f557dc5183
Saito Hughes <68065725+Saitohughes@users.noreply.github.com>
2020-07-20 12:57:51 -0400

    Definicon del movimiento

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed; //variabñle que va a controlar la velocidad del personaje
+    [SerializeField] Rigidbody myRig; //componente necesaria
+    Vector3 mov; //vector de movimiento
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myRig =gameObject.GetComponent<Rigidbody>(); //Se define el valor de la componente que vamos a modificar 
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+    void FixedUpdate()
+    {
+        mov = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed); //le damos el valor al vector con respecto a las direcciones
+        myRig.velocity = mov; //aqui usamos Velocity para darle el vector que ya definimos
+    }
+}
