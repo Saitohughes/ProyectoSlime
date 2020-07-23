ea6a2d3b5f4a43f1b7dfa79e23e7e4ddc41096c9
Santiago Cortes <47002827+Santiago350@users.noreply.github.com>
2020-07-23 16:08:14 -0500

    FriendAI creada y GameController

---
+++ Assets/Scripts/Logic/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] GameObject friend;
+    void Start()
+    {
+        friend = GameObject.FindGameObjectWithTag("Friend");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            friend.GetComponent<FriendAI>().StateModification(1);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            friend.GetComponent<FriendAI>().StateModification(2);
+        }
+    }
+}
