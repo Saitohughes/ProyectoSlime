37bab13d6015be2815848ea5b201a602c21061e3
Saito Hughes <68065725+Saitohughes@users.noreply.github.com>
2020-07-23 16:19:21 -0400

    cajaj agregada falta fixeo

---
+++ Assets/Scripts/BoxMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoxMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    PlayerMovement playerMov;
+    Rigidbody myRig;
+    BoxCollider myCollider;
+
+    
+    private void Awake()
+    {
+          myCollider = GetComponent<BoxCollider>();
+          playerMov = FindObjectOfType<PlayerMovement>();
+          myRig = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Grab(GameObject player) //metodo que se encarga de agarrar la caja
+    {
+        if (player != null && myRig != null)
+        {
+            gameObject.transform.parent = player.transform;
+            playerMov.ChangeSpeed(2);
+            gameObject.layer = 9; 
+            //myRig.isKinematic = false;
+        }
+    }
+
+    public void NoGrab() //funcion que se encarga de soltar la caja
+    {
+        if(myRig != null) 
+        {
+            gameObject.transform.parent = null;
+            playerMov.RestoreSpeed();
+            gameObject.layer = 11;
+            //myRig.isKinematic = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed; //variabñle que va a controlar la velocidad del personaje
+    [SerializeField] float speed, originalSpeed; //variabñle que va a controlar la velocidad del personaje
     [SerializeField] Rigidbody myRig; //componente necesaria
     Vector3 mov; //vector de movimiento
     // Start is called before the first frame update
     private void Awake()
     {
+        originalSpeed = speed;
         myRig =gameObject.GetComponent<Rigidbody>(); //Se define el valor de la componente que vamos a modificar 
     }
     void Start()
@@ -24,5 +25,14 @@ public class PlayerMovement : MonoBehaviour
     {
         mov = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed); //le damos el valor al vector con respecto a las direcciones
         myRig.velocity = mov; //aqui usamos Velocity para darle el vector que ya definimos
+    }
+
+    public void ChangeSpeed(float changeSpeed = 5.5f)
+    {
+         speed = changeSpeed;
+    }
+    public void RestoreSpeed()
+    {
+        speed = originalSpeed;
     }
 }
