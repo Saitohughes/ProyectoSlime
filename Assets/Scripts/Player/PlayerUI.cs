8e01724aef3965e1c6d44e4fb544623e364ebffe
Santiago Cortes <47002827+Santiago350@users.noreply.github.com>
2020-08-26 00:06:41 -0500

    Raycast

---
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField] Image box;
+    [SerializeField] Image metal;
+    [SerializeField] Image bell;
+    [SerializeField] Image key;
+
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    public void Update()
+    {
+        if(player.GetComponent<PlayerController>().GetInstrucction() == "Box")
+        {
+            box.enabled = true;
+        }
+        else if(player.GetComponent<PlayerController>().GetInstrucction() == "Bell")
+        {
+            bell.enabled = true;
+        }
+        else if (player.GetComponent<PlayerController>().GetInstrucction() == "Key")
+        {
+            key.enabled = true;
+        }
+        else if (player.GetComponent<PlayerController>().GetInstrucction() == "Metal")
+        {
+            metal.enabled = true;
+        }
+        else
+        {
+            box.enabled = false;
+            bell.enabled = false;
+            metal.enabled = false;
+            key.enabled = false;
+        }
+    }
+
+}
