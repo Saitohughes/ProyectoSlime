afa84063d103deb3ea03e0cf6e8c9aad82b78fee
Santiago Cortes <47002827+Santiago350@users.noreply.github.com>
2020-08-28 10:21:45 -0500

    Effectos

---
+++ Assets/Scripts/Effects/EffectsObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsObject : MonoBehaviour
+{
+    [SerializeField] float time;
+
+    [SerializeField] bool shakeCamera;
+    [SerializeField] [Range(0f, 1f)] float duration;
+    [SerializeField] [Range(0f, 1f)] float magnitude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (shakeCamera)
+        {
+            Destroy(gameObject, time);
+        }
+    }
+}
--- Assets/Scripts/Mecanicas/MetalBox.cs
+++ Assets/Scripts/Mecanicas/MetalBox.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MetalBox : MonoBehaviour
 {
+    [SerializeField] ParticleSystem melt;
     // Start is called before the first frame update
     public void Melt()
     {
         gameObject.SetActive(false);
+        Instantiate(melt, transform.position, Quaternion.identity);
         Destroy(gameObject,2f);
     }
 }
