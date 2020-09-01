481ae1efa3d7a6862b559faf1841bf80587fd76e
Saito Hughes <68065725+Saitohughes@users.noreply.github.com>
2020-09-01 15:54:41 -0400

    nuew updates

--- Assets/Scripts/Logic/LevelCount.cs
+++ Assets/Scripts/Logic/LevelCount.cs
@@ -22,7 +22,7 @@ public class LevelCount : MonoBehaviour
         {
             instance = this;
         }
-
+       // PlayerPrefs.DeleteAll();
         DontDestroyOnLoad(gameObject);
         mycount = PlayerPrefs.GetInt("turorialProgress", mycount); ;
     }
