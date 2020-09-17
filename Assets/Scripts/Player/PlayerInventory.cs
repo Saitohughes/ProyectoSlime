f8ee0d267538d6d14c3d3a32c506977ecf3dac60
Nicolás Escobar Espinosa <Escobar.Espinosa.Nicolas@gmail.com>
2020-09-17 18:50:45 -0500

    Interfaz de la tienda

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] private float myMoney;
+    [SerializeField] private int[] inventory;
+
+    public static PlayerInventory instance;
+
+    public float MyMoney { get => myMoney; set => myMoney = value; }
+    public int[] Inventory { get => inventory; set => inventory = value; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+
+#if UNITY_EDITOR
+        PlayerPrefs.SetInt("myMoney", 0);
+        PlayerPrefs.SetFloat("myMoney", MyMoney);
+
+        PlayerPrefs.SetInt("mtInventory", 0);
+        PlayerPrefs.SetInt("mtInventory", inventory[0]);
+
+        PlayerPrefs.SetInt("smInventory", 0);
+        PlayerPrefs.SetInt("smInventory", inventory[1]);
+
+        PlayerPrefs.SetInt("gmInventory", 0);
+        PlayerPrefs.SetInt("gmInventory", inventory[2]);
+#endif
+
+        MyMoney = PlayerPrefs.GetFloat("myMoney");
+        inventory[0] = PlayerPrefs.GetInt("mtInventory");
+        inventory[1] = PlayerPrefs.GetInt("smInventory");
+        inventory[2] = PlayerPrefs.GetInt("gmInventory");
+    }
+}
---
+++ Assets/Scripts/UI/ShopManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopManager : MonoBehaviour
+{
+    [Header("Componentes de la Tienda")]
+    [SerializeField] private PlayerInventory myInventory;
+    [SerializeField] private TextMeshProUGUI myMoney;
+
+    [Header("Información Productos")]
+    [SerializeField] private int[] price;
+    [SerializeField] private TextMeshProUGUI[] priceTxt;
+    [SerializeField] private TextMeshProUGUI[] inventory;
+
+
+    public void Awake()
+    {
+        Actualize();
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < priceTxt.Length; i++)
+        {
+            if(myInventory.MyMoney >= price[i])
+            {
+                priceTxt[i].color = Color.white;
+            }
+            else
+            {
+                priceTxt[i].color = Color.red;
+            }
+        }
+    }
+
+    public void Actualize()
+    {
+        myInventory = FindObjectOfType<PlayerInventory>();
+        myMoney.text = myInventory.MyMoney.ToString();
+
+        for (int i = 0; i < myInventory.Inventory.Length; i++)
+        {
+            priceTxt[i].text = price[i].ToString();
+            inventory[i].text = ("x" + myInventory.Inventory[i]);
+        }
+    }
+
+    public void Buy(int handicapSelected)
+    {
+        if (myInventory.MyMoney >= price[handicapSelected])
+        {
+            myInventory.MyMoney -= price[handicapSelected];
+            myInventory.Inventory[handicapSelected] += 1;
+
+            Actualize(); 
+        }
+    }
+}
