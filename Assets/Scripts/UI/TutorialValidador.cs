30bfea8af4a7ff12c73eb1e44a24df64b023ca67
Santiago Cortes <47002827+Santiago350@users.noreply.github.com>
2020-10-27 20:22:17 -0500

    Tutorial tienda

---
+++ Assets/Scripts/UI/TutorialValidador.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialValidador : MonoBehaviour
+{
+    public static TutorialValidador instance;
+    public static TutorialValidador Instance { get => instance; }
+    [SerializeField] int count;
+    [SerializeField] GameObject tutorial;
+    public int Count { get => count; set => count = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
+        Count = PlayerPrefs.GetInt("Validador_tutorial");
+        if (Count == 0)
+        {
+            ChangeValidator();
+        }
+#if UNITY_EDITOR
+        //ClearValidator();
+#endif
+    }
+
+    // Update is called once per frame
+    public void ChangeValidator()
+    {
+        Count = 1;
+        tutorial.SetActive(true);
+        PlayerPrefs.SetInt("Validador_tutorial", Count);
+    }
+
+    public void ClearValidator()
+    {
+        PlayerPrefs.SetInt("Validador_tutorial", 0);
+    }
+}
